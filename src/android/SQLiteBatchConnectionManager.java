de18f4467f6851af82e0be9414e66824ba3c324c
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-05-20 13:17:32 -0400

    updates from cordova-plugin-sqlite-batch-connection-manager-core

--- src/android/SQLiteBatchConnectionManager.java
+++ src/android/SQLiteBatchConnectionManager.java
@@ -2,19 +2,62 @@
 
 package io.sqlc;
 
-import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class SQLiteBatchConnectionManager extends CordovaPlugin {
   @Override
-  public boolean execute(String action, JSONArray args, CallbackContext cbc) {
-    switch (action) {
+  public boolean execute(String action, JSONArray data, CallbackContext cbc) {
+    switch(action) {
       case "openDatabaseConnection":
-        cbc.success(args);
-      return true;
+        openDatabaseConnection(data, cbc);
+        break;
+      case "executeBatch":
+        executeBatch(data, cbc);
+        break;
+      default:
+        return false;
     }
-    return false;
+    return true;
+  }
+
+  static private void
+  openDatabaseConnection(JSONArray args, CallbackContext cbc) {
+    try {
+      final JSONObject options = args.getJSONObject(0);
+
+      final String fullName = options.getString("fullName");
+
+      final int flags = options.getInt("flags");
+
+      final int mydbc = SQLiteBatchCore.openBatchConnection(fullName, flags);
+
+      if (mydbc < 0) {
+        cbc.error("open error: " + -mydbc);
+      } else {
+        cbc.success(mydbc);
+      }
+    } catch(Exception e) {
+      // NOT EXPECTED - internal error:
+      cbc.error(e.toString());
+    }
+  }
+
+  static private void executeBatch(JSONArray args, CallbackContext cbc) {
+    try {
+      final int mydbc = args.getInt(0);
+
+      JSONArray data = args.getJSONArray(1);
+
+      JSONArray results = SQLiteBatchCore.executeBatch(mydbc, data);
+
+      cbc.success(results);
+    } catch(Exception e) {
+      // NOT EXPECTED - internal error:
+      cbc.error(e.toString());
+    }
   }
 }
